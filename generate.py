7aac4517bf29960bfeba46e6ed9b02aba2733c58
darwinj07 <54761229+darwinj07@users.noreply.github.com>
2023-10-17 01:52:02 +0900

    use new methods for gradio component generation

--- generate.py
+++ generate.py
@@ -161,31 +161,31 @@ def main(
     gr.Interface(
         fn=evaluate,
         inputs=[
-            gr.components.Textbox(
+            gr.Textbox(
                 lines=2,
                 label="Instruction",
                 placeholder="Tell me about alpacas.",
             ),
-            gr.components.Textbox(lines=2, label="Input", placeholder="none"),
-            gr.components.Slider(
+            gr.Textbox(lines=2, label="Input", placeholder="none"),
+            gr.Slider(
                 minimum=0, maximum=1, value=0.1, label="Temperature"
             ),
-            gr.components.Slider(
+            gr.Slider(
                 minimum=0, maximum=1, value=0.75, label="Top p"
             ),
-            gr.components.Slider(
+            gr.Slider(
                 minimum=0, maximum=100, step=1, value=40, label="Top k"
             ),
-            gr.components.Slider(
+            gr.Slider(
                 minimum=1, maximum=4, step=1, value=4, label="Beams"
             ),
-            gr.components.Slider(
+            gr.Slider(
                 minimum=1, maximum=2000, step=1, value=128, label="Max tokens"
             ),
-            gr.components.Checkbox(label="Stream output"),
+            gr.Checkbox(label="Stream output"),
         ],
         outputs=[
-            gr.inputs.Textbox(
+            gr.Textbox(
                 lines=5,
                 label="Output",
             )
